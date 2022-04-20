58100a72ce6d8e59a884358490b4abd186df75a6
Altilunium <70379302+altilunium@users.noreply.github.com>
2022-04-20 15:11:25 +0700

    Bugfix

--- timer.py
+++ timer.py
@@ -4,24 +4,19 @@ import sys
 import time
 import pyperclip
 import threading
-
+import signal
 
 global timeToStop
 global delta_fix
 global now
 timeToStop = False
 
-class thread(threading.Thread):
-	def __init__(self):
-		threading.Thread.__init__(self)
-	def run(self):
-		global timeToStop
-		while True:
-			a = raw_input()
-			if a == "q":
-				timeToStop = True
-			else:
-				print "-->" + a
+		
+
+def sig_handler(sig, frame):
+	global timeToStop
+	print("Exiting..")
+	timeToStop = True
 
 
 
@@ -38,17 +33,18 @@ def showCurrentDuration(start):
 	delta_second = delta_second - (delta_minute * 60)
 	delta_fix = str(delta_hours) + " hour " + str(delta_minute) + " minute " + str(delta_second) + " second"
 	os.system('cls')
-	print "reitnorF Timer"
-	print "(Press q and enter to stop the timer)"
-	print "Started: "+ start.strftime("%d %B %Y - %H:%M")
-	print delta_fix
+	print("reitnorF Timer")
+	print("(Press ctrl+c to stop the timer)")
+	print("Started: "+ start.strftime("%d %B %Y - %H:%M"))
+	print(delta_fix)
 	time.sleep(1)
 
 os.system('cls')
 begin = datetime.datetime.now()
 
-observer = thread()
-observer.start()
+
+signal.signal(signal.SIGINT, sig_handler)
+
 while not timeToStop:
 	showCurrentDuration(begin)	
 
@@ -57,6 +53,5 @@ while not timeToStop:
 
 string_report = "[" + begin.strftime("%d %B %Y") + "] " + begin.strftime("%H:%M") + "-"+ now.strftime("%H:%M") + " (" + delta_fix + ")"
 pyperclip.copy(string_report)
-print string_report
-print "Report copied to your clipboard"
-print "Press f to exit"
+print(string_report)
+print("Report copied to your clipboard")
